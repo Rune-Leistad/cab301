50f5bc857915c88c65accd3adb1ed21e947a07ff
exlipy <41883655+exlipy@users.noreply.github.com>
2019-05-08 15:36:46 +1000

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -6,16 +6,19 @@ public class TestClass {
     @Test
     public void testMinDistance() {
         int[] array = {0, 500, 1000, -501, 10};
-        assertEquals(MainAss2.minDistance(array), 10);
-        assertEquals(MainAss2.minDistance2(array), 10);
+        int expected = 10;
+        assertEquals(expected, MainAss2.minDistance(array));
+        assertEquals(expected, MainAss2.minDistance2(array));
 
         int[] array2 = {0, -1, -2, -3, 1, 2, 3, 4};
-        assertEquals(MainAss2.minDistance(array2), 1);
-        assertEquals(MainAss2.minDistance2(array2), 1);
+        expected = 1;
+        assertEquals(expected, MainAss2.minDistance(array2));
+        assertEquals(expected, MainAss2.minDistance2(array2));
 
         int[] array3 = {0, 0, 0, 1, 2, 3, 4};
-        assertEquals(MainAss2.minDistance(array3), 0);
-        assertEquals(MainAss2.minDistance2(array3), 0);
+        expected = 0;
+        assertEquals(expected, MainAss2.minDistance(array3));
+        assertEquals(expected, MainAss2.minDistance2(array3));
 
     }
 }
